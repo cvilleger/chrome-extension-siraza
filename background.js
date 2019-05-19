38061aa36681dcdf4cbd88677603fafb2b7702f2
cvilleger <villeger.c@gmail.com>
2019-05-19 21:14:52 +0200

    init

--- background.js
+++ background.js
@@ -1,1 +1,20 @@
-'use strict';
+var tickRate = 60000; // 1min
+
+function checkStream() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.twitch.tv/helix/streams?user_login=siraza", true)
+  xhr.onreadystatechange = function () {
+    if(xhr.readyState === 4) {
+      var data = JSON.parse(xhr.responseText);
+      if(data["stream"] === null){
+        chrome.browserAction.setIcon({path:"images/icon-red.png"})
+      }else{
+        chrome.browserAction.setIcon({path:"images/icon-green.png"})
+      }
+      setTimeout(checkStream, tickRate)
+    }
+  };
+  xhr.send()
+}
+
+checkStream();
