36df7e4857958bc2dd48e38cfc25d7b9c67e07bf
cvilleger <villeger.c@gmail.com>
2019-05-19 21:44:52 +0200

    init

--- background.js
+++ background.js
@@ -15,13 +15,14 @@ function notify() {
 function checkStream() {
   let xhr = new XMLHttpRequest();
   xhr.open("GET", "https://api.twitch.tv/helix/streams?user_login=siraza", true)
+  xhr.setRequestHeader('Client-ID', '4s5z6mqls50fwh5w42ig1zlfmrv5d4');
   xhr.onreadystatechange = function () {
     if(xhr.readyState === 4) {
       let data = JSON.parse(xhr.responseText);
       if(data["stream"] === null){
-        chrome.browserAction.setIcon({path:"images/icon-red.png"})
+        chrome.browserAction.setIcon({path:"images/icon-red.png"});
       }else{
-        chrome.browserAction.setIcon({path:"images/icon-green.png"})
+        chrome.browserAction.setIcon({path:"images/icon-green.png"});
         // notify();
       }
       setTimeout(checkStream, tickRate)
