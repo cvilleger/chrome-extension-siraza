610897c3b11715c1665f37c36066604be80df0f2
cvilleger <villeger.c@gmail.com>
2019-05-19 21:37:03 +0200

    init

--- background.js
+++ background.js
@@ -1,15 +1,28 @@
-var tickRate = 60000; // 1min
+const tickRate = 60000; // 1min
+
+function notify() {
+  if (Notification.permission === "granted") {
+    new Notification("Sir-Aza is now live !");
+  } else {
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
+        new Notification("Sir-Aza is now live !");
+      }
+    });
+  }
+}
 
 function checkStream() {
-  var xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   xhr.open("GET", "https://api.twitch.tv/helix/streams?user_login=siraza", true)
   xhr.onreadystatechange = function () {
     if(xhr.readyState === 4) {
-      var data = JSON.parse(xhr.responseText);
+      let data = JSON.parse(xhr.responseText);
       if(data["stream"] === null){
         chrome.browserAction.setIcon({path:"images/icon-red.png"})
       }else{
         chrome.browserAction.setIcon({path:"images/icon-green.png"})
+        // notify();
       }
       setTimeout(checkStream, tickRate)
     }
