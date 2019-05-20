9a7488f4b64592ff0ede1e3d92e78cb98eccc036
cvilleger <villeger.c@gmail.com>
2019-05-20 14:35:52 +0200

    Rework

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# chrome-extension-siraza
-
 ## How to install
+
+### Developer mode
 
 1. Download master release : [Master Release](https://github.com/cvilleger/chrome-extension-siraza/archive/master.zip)
 2. Unzip the file downloaded file
 3. Visit `chrome://extensions` and enable **Developer mode** by ticking the checkbox in the upper-right corner.
 4. Click on the "**Load unpacked extension...**" button and select the directory containing your unpacked extension.
+
+### Chrome Web Store
--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 const tickRate = 60000; // 1min
+const xhr = new XMLHttpRequest();
 
 function notify() {
   if (Notification.permission === "granted") {
@@ -13,11 +14,10 @@ function notify() {
 }
 
 function checkStream() {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.twitch.tv/helix/streams?user_login=siraza", true)
+  xhr.open("GET", "https://api.twitch.tv/helix/streams?user_login=siraza", true);
   xhr.setRequestHeader('Client-ID', '4s5z6mqls50fwh5w42ig1zlfmrv5d4');
   xhr.onreadystatechange = function () {
-    if(xhr.readyState === 4 && xhr.status === 200) {
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
       let data = JSON.parse(xhr.responseText);
       if(data["data"].length > 0){
         chrome.browserAction.setIcon({path:"images/icon-green.png"});
