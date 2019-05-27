d64cc00e6d8cd7e1024e24a25292e002560e5e50
cvilleger <villeger.c@gmail.com>
2019-05-28 01:00:05 +0200

    refacto by removing iframe

--- popup.js
+++ popup.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const xhr = new XMLHttpRequest();
+
+function reqListener () {
+    let data = JSON.parse(this.response)['data'];
+
+    if (0 === data.length){
+        document.querySelector('#title img').src = 'images/icon-red.png';
+    } else {
+        document.querySelector('#title img').src = 'images/icon-green.png';
+    }
+}
+
+xhr.addEventListener("load", reqListener);
+xhr.open("GET", "https://api.twitch.tv/helix/streams?user_login=siraza");
+xhr.setRequestHeader('Client-ID', '4s5z6mqls50fwh5w42ig1zlfmrv5d4');
+xhr.send();
+
+window.addEventListener("DOMContentLoaded", (event) => {
+
+});
